036a8b0636a75057c471f2dbf9248596eb1f3514
MaksimReyda <reida.maksim@gmail.com>
2022-04-06 21:20:09 +0300

    lesson-10

---
+++ cypress/integration/contact-us.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("Test Contact Us form via WebdriverUni", () => {
+
+    it("Should be able to submit", () => {
+
+    })
+
+    it("Should NOT be able to submit", () => {
+        
+    })
+})
