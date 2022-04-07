9d37276af17de1734961cb78b474748118cee85e
MaksimReyda <reida.maksim@gmail.com>
2022-04-07 19:40:35 +0300

    Visit & Click Command

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -3,7 +3,8 @@
 describe("Test Contact Us form via WebdriverUni", () => {
 
     it("Should be able to submit", () => {
-
+        cy.visit('http://www.webdriveruniversity.com/index.html')
+        cy.get('#contact-us > .thumbnail').click()
     })
 
     it("Should NOT be able to submit", () => {
