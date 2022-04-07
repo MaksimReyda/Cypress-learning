7767e3b06d5976c835613c6dde57ccc24077ce46
MaksimReyda <reida.maksim@gmail.com>
2022-04-07 19:54:27 +0300

    Type Command

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -3,8 +3,12 @@
 describe("Test Contact Us form via WebdriverUni", () => {
 
     it("Should be able to submit", () => {
-        cy.visit('http://www.webdriveruniversity.com/index.html')
-        cy.get('#contact-us > .thumbnail').click()
+        cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.get('[name="first_name"]').type('Maksym')
+        cy.get('[name="last_name"]').type('Reida')
+        cy.get('[name="email"]').type('test@mail.com')
+        cy.get('textarea.feedback-input').type('some test text in comments')
+
     })
 
     it("Should NOT be able to submit", () => {
