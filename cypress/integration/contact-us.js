f832f0b849d665730f3e914b3ade0d0171c0ea17
MaksimReyda <reida.maksim@gmail.com>
2022-04-09 09:07:38 +0300

    Negative scenario

--- cypress/integration/contact-us.js
+++ cypress/integration/contact-us.js
@@ -2,16 +2,23 @@
 
 describe("Test Contact Us form via WebdriverUni", () => {
 
-    it("Should be able to submit", () => {
+    beforeEach(()=>{
         cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+    })
+
+    it("Should be able to submit", () => {
         cy.get('[name="first_name"]').type('Maksym')
         cy.get('[name="last_name"]').type('Reida')
         cy.get('[name="email"]').type('test@mail.com')
         cy.get('textarea.feedback-input').type('some test text in comments')
-
+        cy.get('[type="submit"]').click()
     })
 
     it("Should NOT be able to submit", () => {
+        cy.get('[name="first_name"]').type('Maksym')
+        cy.get('[name="last_name"]').type('Reida')
         
+        cy.get('textarea.feedback-input').type('some test text in comments')
+        cy.get('[type="submit"]').click()
     })
 })
