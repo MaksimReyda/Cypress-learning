81c9611af828a84a85482a6f639ac53ff5e12028
MaksimReyda <reida.maksim@gmail.com>
2022-04-10 10:39:00 +0300

    Submit contact us form

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -7,6 +7,10 @@ describe("Test Contact Us form via Automation Test Store", () => {
     })
 
     it("Should be able to submit", () => {
-
+        cy.get('.info_links_footer > :nth-child(5) > a').click()
+        cy.get('#ContactUsFrm_first_name').type('Maksym')
+        cy.get('#ContactUsFrm_email').type('test@email.com')
+        cy.get('#ContactUsFrm_enquiry').type('Some text Some textSome text')
+        cy.contains('Submit').click()
     })
 })
