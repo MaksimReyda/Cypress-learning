196f202a366da63bf29760cb1d59bb0e689e72b3
MaksimReyda <reida.maksim@gmail.com>
2022-04-29 08:35:36 +0300

    contains command

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,12 +1,15 @@
 /// <reference types="cypress" />
 
+
+
+
 describe("Test Contact Us form via WebdriverUni", () => {
 
     beforeEach(()=>{
         cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
     })
 
-    it.only("Should be able to submit", () => {
+    it("Should be able to submit", () => {
         cy.get('[name="first_name"]').type('Maksym')
         cy.get('[name="last_name"]').type('Reida')
         cy.get('[name="email"]').type('test@mail.com')
@@ -22,5 +25,6 @@ describe("Test Contact Us form via WebdriverUni", () => {
         
         cy.get('textarea.feedback-input').type('some test text in comments')
         cy.get('[type="submit"]').click()
+        cy.get('body').contains('Error: all fields are required')
     })
 })
